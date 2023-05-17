2d20a596b23bcca2762a3d1d2006ef1613a1ece5
inah <67313423+nanahb@users.noreply.github.com>
2023-05-17 07:05:28 +0000

    Added React

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+console.log("AHAJDAIJOISAJDOSJD")
+const App = () => {
+  return <h1>Hello, React!</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
